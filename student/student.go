3f8879321b835d0207a771027afd8d7d33d55dc8
Ekaterina <kacaderevcova@gmail.com>
2024-09-28 15:36:46 +0200

    add-constructors

--- student/student.go
+++ student/student.go
@@ -6,3 +6,12 @@ type Student struct {
 	Year   int
 	Scores []int
 }
+
+func NewStudent(id int, name string, year int, scores []int) *Student {
+	return &Student{
+		Id:     id,
+		Name:   name,
+		Year:   year,
+		Scores: scores,
+	}
+}
