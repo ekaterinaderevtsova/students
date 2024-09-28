a1e0d9d1a3aacbb04143b624527ad747644a29e2
Ekaterina <kacaderevcova@gmail.com>
2024-09-28 14:55:32 +0200

    add-delete

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	students.AddStudent(student.Student{Id: students.GetNumStudents() + 1, Name: "Jake", Year: 4, Scores: []int{4, 4, 5}})
 	students.AddStudent(student.Student{Id: students.GetNumStudents() + 1, Name: "Finn", Year: 3, Scores: []int{5, 3, 5}})
 	students.AddStudent(student.Student{Id: students.GetNumStudents() + 1, Name: "Bubblegum", Year: 6, Scores: []int{5, 5, 5, 5, 5}})
+	students.RemoveStudentById(4)
 	students.DisplayStudents()
 }
